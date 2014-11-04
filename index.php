ff59b931cc16109183393427b1cdbf78304ea2c2
unknown <Mathieu@MATHIEULAPTOP.instituutvivere.local>
2014-11-04 11:47:14 +0100

    Added invite list

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ try {
     $db->query("SET SESSION sql_mode = 'ANSI,ONLY_FULL_GROUP_BY'");
 
 
-    $QueryInventations = "SELECT * FROM challenges
+    $QueryInvit = "SELECT * FROM challenges
                           LEFT JOIN users ON challenges.challenger_user_ID = users.ID
                           WHERE challenged_user_ID = '1'";
 
-    $StIncertations = $db->prepare($QueryInventations);
-    $StIncertations->execute();
+    $StInvite = $db->prepare($QueryInvite);
+    $StInvite->execute();
 
     while ($aRow = $StIncertations->fetch(PDO::FETCH_ASSOC)) {
         echo $aRow["username"];
