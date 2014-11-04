6ae581c7b108b6c6a50fddd0d112bd28375ef180
Armand Mahieu <badkarma@ziggo.nl>
2014-11-04 16:12:44 +0100

    Done(DB fixed)

--- uitdagen.php
+++ uitdagen.php
@@ -5,8 +5,10 @@
  * Date: 04-11-2014
  */
 
-
 require_once"dbconnect.php";
+
+//Tijdelijk
+$tempuserid = 55;
 
 ?>
 
@@ -15,19 +17,19 @@ require_once"dbconnect.php";
     <br>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <!-- Naam van tegenstander -->
-        <label for="tegenstander">Voer hier de naam van je tegenspeler in</label>
+        <label for="tegenstander">Type the name of your opponent</label>
         <br>
         <input id="tegenstander" name="Tegenstander" placeholder="Tegenspeler" required="" type="text">
         <br><br>
 
         <!-- Uiterst aantal dagen van actieve uitnodiging -->
-        <label for="verloopdatum">Aantal dagen dat de uitnodiging actief blijft</label>
+        <label for="verloopdatum">Amount of days that the challenge stays available</label>
         <br>
         <input id="verloopdatum" name="Verloopdagen" placeholder="3"  max="7" type="number">
         <br><br>
 
         <!-- Zet kiezen -->
-        <label for="zet">Kies je zet</label>
+        <label for="zet">Choose your challenge</label>
         <br>
         <input type="radio" name="Zet" value="rock"><img src="img/rock.png">
         <input type="radio" name="Zet" value="paper"><img src="img/paper.png">
@@ -37,11 +39,14 @@ require_once"dbconnect.php";
         <br><br>
         <br>
         <br>
-        <input type="submit" value="Daag uit!"/>
+        <input type="submit" value="Challenge!"/>
         <br>
         <br>
     </form>
 <?php
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["Tegenstander"])) {
         if (isset($_POST["Verloopdagen"])) {
@@ -49,36 +54,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $tegenstander = $_POST["Tegenstander"];
                 $verloopdagen = $_POST["Verloopdagen"];
 
-                if ($_POST["Zet"] == "rock" ) {
+                if (isset($_POST["Zet"]) == "rock" ) {
                     $eerstezet = 1;
-                } elseif ($_POST["Zet"] == "paper") {
+                    echo "You have selected Rock";
+                } elseif (isset($_POST["Zet"]) == "paper") {
                     $eerstezet = 2;
-                } elseif ($_POST["Zet"] == "scissors") {
+                    echo "You have selected Paper";
+                } elseif (isset($_POST["Zet"]) == "scissors") {
                     $eerstezet = 3;
-                } elseif ($_POST["Zet"] == "lizard") {
+                    echo "You have selected Scissors";
+                } elseif (isset($_POST["Zet"]) == "lizard") {
                     $eerstezet = 4;
-                } elseif ($_POST["Zet"] == "spock") {
+                    echo "You have selected Lizard";
+                } elseif (isset($_POST["Zet"]) == "spock") {
                     $eerstezet = 5;
+                    echo "You have selected Spock";
                 }
-
+                echo " and are playing against: $tegenstander";
             }
         }
     }
 }
 
+
 try {
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->query("SET SESSION sql_mode = 'ANSI, ONLY_FULL_GROUP_BY'");
+    //$db->query("SET SESSION sql_mode = 'ANSI, ONLY_FULL_GROUP_BY'");
+
+    //Challenger ID opvragen
+    $QuerychallengerID = "SELECT ID FROM users WHERE username = '$tegenstander'";
+    $stChallengerID = $db->prepare($QuerychallengerID);
+    $stChallengerID->execute();
+
+    while($aRow = $stChallengerID->fetch(PDO::FETCH_ASSOC))
+    {
+        $tegenstanderid = $aRow["ID"];
+    }
 
 
-    $QueryChallenge = "INSERT INTO challenges(create_date, active, expiration_date, challenger_user_ID, challenger_move, challenged_user_ID, challenged_move) VALUES (NOW(),0,$datum,$userid,$eerstezet,(SELECT ID FROM users WHERE username = '$tegenstander'),0)";
+    $QueryChallenge = "INSERT INTO challenges (create_date, active, expiration_date, challenger_user_ID, challenger_move, challenged_user_ID, challenged_move) VALUES (NOW(),1,$verloopdagen,$tempuserid,$eerstezet, $tegenstanderid,0)";
     $stChallenge = $db->prepare($QueryChallenge);
     $stChallenge->execute();
 
-    $id = $db->lastInsertId();
 
-    echo $id;
+
+    $id = $db->lastInsertId();
 
 
 }
