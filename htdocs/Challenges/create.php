d427c68d17545ff0280844f7b61004975305ffc9
Armand Mahieu <badkarma@ziggo.nl>
2014-11-24 13:26:08 +0100

    Re-added if statement in create.php

--- htdocs/Challenges/create.php
+++ htdocs/Challenges/create.php
@@ -5,36 +5,36 @@
  * Date: 04-11-2014
  */
 
-require_once "dbconnect.php";
-require "index.php";
+require_once"dbconnect.php";
+require"index.php";
 
 
 if(isset($_SESSION['user'])) {
     $username = $_SESSION['user'];
 }
 
-$tegenstanderid = null;
-$eerstezet = null;
+
 ?>
-Terug naar <a href="../index.php">Home</a>
+<body>
+Terug naar <a href="index.php">Home</a>
 <p>
-    <h1>Daag iemand uit</h1>
+<h1>Daag iemand uit</h1>
 
 
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <!-- Naam van tegenstander -->
-        <label for="tegenstander">Type the name of your opponent</label>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <!-- Naam van tegenstander -->
+    <label for="tegenstander">Type the name of your opponent</label>
 
-        <input id="tegenstander" name="Tegenstander" placeholder="Tegenspeler" required="" type="text">
-</p>
-<p>
+    <input id="tegenstander" name="Tegenstander" placeholder="Tegenspeler" required="" type="text">
+    </p>
+    <p>
         <!-- Uiterst aantal dagen van actieve uitnodiging --
         <label for="verloopdatum">Amount of days that the challenge stays available</label>
 
         <input id="verloopdatum" name="Verloopdagen" placeholder="3"  max="7" type="number">
         -->
-</p>
-        <p>
+    </p>
+    <p>
         <!-- Zet kiezen -->
         <label for="lbzet">Choose your challenge<br></label>
 
@@ -42,23 +42,23 @@ Terug naar <a href="../index.php">Home</a>
 
         <label class="button">
             <input id="zet" type="radio" name="Zet" value="rock" />
-            <img src="../img/rock.png">
+            <img src="img/rock.png">
         </label>
         <label class="button">
             <input id="zet" type="radio" name="Zet" value="paper" />
-            <img src="../img/paper.png">
+            <img src="img/paper.png">
         </label>
         <label class="button">
             <input id="zet" type="radio" name="Zet" value="scissors" />
-            <img src="../img/scissors.png">
+            <img src="img/scissors.png">
         </label>
         <label class="button">
             <input id="zet" type="radio" name="Zet" value="lizard" />
-            <img src="../img/lizard.png">
+            <img src="img/lizard.png">
         </label>
         <label class="button">
             <input id="zet" type="radio" name="Zet" value="spock" />
-            <img src="../img/spock.png">
+            <img src="img/spock.png">
         </label>
 
 
@@ -75,7 +75,7 @@ Terug naar <a href="../index.php">Home</a>
         <input type="submit" value="Challenge!"/>
 
 
-    </form>
+</form>
 
 <?php
 
@@ -143,10 +143,18 @@ try {
     }
 
     $date = date('YmdHi');
+    if (isset($_SERVER["REQUEST_METHOD"]) == "POST") {
+        if (isset($_POST["Tegenstander"])) {
+            if (isset($_POST["Verloopdagen"])) {
+                if (isset($_POST["Zet"])) {
+                    $QueryChallenge = "INSERT INTO challenges (create_date, active, expiration_date, challenger_user_ID, challenger_move, challenged_user_ID, challenged_move) VALUES ($date,1,3,$EigenID,$eerstezet, $tegenstanderid,0)";
+                    $stChallenge = $db->prepare($QueryChallenge);
+                    $stChallenge->execute();
+                }
+            }
+        }
+    }
 
-    $QueryChallenge = "INSERT INTO challenges (create_date, active, expiration_date, challenger_user_ID, challenger_move, challenged_user_ID, challenged_move) VALUES ($date,1,3,$EigenID,$eerstezet, $tegenstanderid,0)";
-    $stChallenge = $db->prepare($QueryChallenge);
-    $stChallenge->execute();
 
 
 
@@ -166,3 +174,4 @@ catch(PDOException $e)
     trigger_error($sMsg);
 }
 ?>
+</body>
