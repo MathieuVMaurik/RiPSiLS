5078f1bfc3c6e84771013d7356ba00f2daa926f2
Armand Mahieu <badkarma@ziggo.nl>
2015-01-13 10:57:43 +0100

    sent.php: Delete button fixed; Difference between sent and menu. create.script.php: Changed header location.

--- RiPSiLS/challenges/create.script.php
+++ RiPSiLS/challenges/create.script.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $challengeeID = getChallengeeID($challengeeName);
     $moveID = getMoveID($moveName);
     createChallenge($challengerID, $challengeeID, $moveID);
-    header("location: ../main/main.php?Sent");
+    header("location: ../main/main.php?Sent&challenge");
 }
 
 ?>
--- RiPSiLS/challenges/sent.php
+++ RiPSiLS/challenges/sent.php
@@ -4,29 +4,42 @@
  * Published for RiPSiLS
  * Date: 10-11-2014
  */
-?>
-<h2>challenge sent, here is a list of send challenges</h2>
-<p>
-    <?php
+
+if(isset($_GET['challenge'])) {
+
+    echo '
+    <h2>Challenge sent, here is a list of sent challenges</h2>';
+
+}
+else{
+    echo '<h2>History</h2>';
+}
     if(isset($_SESSION['user'])) {
         $username = $_SESSION['user'];
     }
 
-
-    ?>
-</p>
-<?php
 try {
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-if (isset($_SERVER["REQUEST_METHOD"]) == "POST") {
-    if (isset($_POST["Challengedel"])) {
-        $QueryChallengedel = "UPDATE challenges SET active=0 WHERE ID=$challengedelid";
+    function Deletegame($DeleteID)
+    {
+        global $db;
+        $QueryChallengedel = "UPDATE challenges SET active=0 WHERE ID=$DeleteID";
         $stChallengedel = $db->prepare($QueryChallengedel);
         $stChallengedel->execute();
     }
+
+if(isset($_POST["delbutton"]))
+{
+    foreach($_POST["delbutton"] as $DeleteID => $DelState)
+    {
+        $DState = implode(" ", $DelState);
+    }
+    Deletegame($DeleteID);
 }
+
+
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
     //Eigen ID
     $QueryownID = "SELECT ID FROM users WHERE username = :username";
@@ -65,131 +78,86 @@ if (isset($_SERVER["REQUEST_METHOD"]) == "POST") {
         }
 
         while ($aRow = $stActivechallenges->fetch(PDO::FETCH_ASSOC)) {
-            $listID = $aRow["ID"];
-            $listYourMove = $aRow["challenger_move"];
-            $listOpposingMove = $aRow["challenged_move"];
-            $listOpposingPlayer = $aRow["challenged_user_ID"];
-            $listActive = $aRow["active"];
-            ?>
-            <tr>
+        $listID = $aRow["ID"];
+        $listYourMove = $aRow["challenger_move"];
+        $listOpposingMove = $aRow["challenged_move"];
+        $listOpposingPlayer = $aRow["challenged_user_ID"];
+        $listActive = $aRow["active"];
+        ?>
+        <tr>
+            <td>
+                <form method="post">
+                <input type="submit" value="delete" name="delbutton[<?php echo $listID ?>][delete]">
+                </form>
+            </td>
+            <td><?php
+                echo $listID;
+                ?>
+            </td>
+
+            <td><?php
+                //Jouw zet
+                if ($listYourMove == 1) {
+                    echo "Rock";
+                } elseif ($listYourMove == 2) {
+                    echo "Paper";
+                } elseif ($listYourMove == 3) {
+                    echo "Scissors";
+                } elseif ($listYourMove == 4) {
+                    echo "Lizard";
+                } elseif ($listYourMove == 5) {
+                    echo "Spock";
+                }
+                ?></td>
+
+            <td><?php
+                //Tegenstanders zet
+                if ($listOpposingMove == 1) {
+                    echo "Rock";
+                } elseif ($listOpposingMove == 2) {
+                    echo "Paper";
+                } elseif ($listOpposingMove == 3) {
+                    echo "Scissors";
+                } elseif ($listOpposingMove == 4) {
+                    echo "Lizard";
+                } elseif ($listOpposingMove == 5) {
+                    echo "Spock";
+                } else {
+                    echo "None";
+                }
+                ?></td>
+
             <?php
-            $count = 1;
-            foreach($aRow as $key => $value) {
-                if($count == 1){
-                    ?><td>
-                    <input type="submit" value= "<?php $listID ?>" name="button"></td>
-                    <td><?php
-                        echo $listID;
-                        ?>
-                    </td>
+            //Tegenstander Naam
+            $QueryoppName = "SELECT username FROM users WHERE ID = :oppid";
+            $stoppName = $db->prepare($QueryoppName);
+            $stoppName->bindParam(':oppid', $listOpposingPlayer, PDO::PARAM_STR);
+            $stoppName->execute();
 
-                    <?php
-
-                    $count++;
-                }
-                ?>
-
-                <?php
-                if($count == 2){
-                    ?>
-                    <td><?php
-                        //Jouw zet
-                        if($listYourMove == 1) {
-                            echo "Rock";
-                        }
-                        elseif($listYourMove == 2) {
-                            echo "Paper";
-                        }
-                        elseif($listYourMove == 3) {
-                            echo "Scissors";
-                        }
-                        elseif($listYourMove == 4) {
-                            echo "Lizard";
-                        }
-                        elseif($listYourMove == 5) {
-                            echo "Spock";
-                        }
-                        ?></td>
-                    <?php
-                    $count++;
-                }
-                ?>
-
-                <?php
-                if($count == 3){
-                    ?>
-                    <td><?php
-                        //Tegenstanders zet
-                        if($listOpposingMove == 1) {
-                            echo "Rock";
-                        }
-                        elseif($listOpposingMove == 2) {
-                            echo "Paper";
-                        }
-                        elseif($listOpposingMove == 3) {
-                            echo "Scissors";
-                        }
-                        elseif($listOpposingMove == 4) {
-                            echo "Lizard";
-                        }
-                        elseif($listOpposingMove == 5) {
-                            echo "Spock";
-                        }
-                        else{ echo "None";}
-                        ?></td>
-                    <?php
-                    $count++;
-                }
-                ?>
-
-                <?php
-                if($count == 4){
-                    //Tegenstander Naam
-                    $QueryoppName = "SELECT username FROM users WHERE ID = :oppid";
-                    $stoppName = $db->prepare($QueryoppName);
-                    $stoppName->bindParam(':oppid', $listOpposingPlayer, PDO::PARAM_STR);
-                    $stoppName->execute();
-
-                    while($cRow = $stoppName->fetch(PDO::FETCH_ASSOC))
-                    {
-                        $OppName = $cRow["username"];
-                    }
-                    ?>
-
-                    <td><?php
-                        echo $OppName;
-                        ?></td>
-                    <?php
-                    $count++;
-
-                }
-                if($count == 5){
-                    ?>
-                    <td><?php
-                        //activiteit
-                        if($listActive == 1) {
-                            echo "Not Yet Answered";
-                        }
-                        elseif($listActive == 2) {
-                            echo "Accepted";
-                        }
-                        elseif($listActive == 0) {
-                            echo "Played";
-                        }
-                        elseif($listActive == 3) {
-                            echo "Declined";
-                        }
-                        ?></td>
-                    <?php
-                    $count++;
-                }
+            while ($cRow = $stoppName->fetch(PDO::FETCH_ASSOC)) {
+                $OppName = $cRow["username"];
             }
             ?>
-            </tr>
-        <?php
-        }
 
-        ?>
+            <td><?php
+                echo $OppName;
+                ?></td>
+
+            <td><?php
+                //activiteit
+                if ($listActive == 1) {
+                    echo "Not Yet Answered";
+                } elseif ($listActive == 2) {
+                    echo "Accepted";
+                } elseif ($listActive == 0) {
+                    echo "Played";
+                } elseif ($listActive == 3) {
+                    echo "Declined";
+                }
+                }
+                        ?></td>
+
+            </tr>
     </tbody>
         <tfoot>
         <tr>
