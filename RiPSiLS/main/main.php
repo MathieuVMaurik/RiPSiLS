4c19ea823a6390c4abb3401088aa617bd0c6492a
William Willing <william@williamwilling.com>
2015-01-19 10:40:59 +0100

    Fixed indentation.

--- RiPSiLS/main/main.php
+++ RiPSiLS/main/main.php
@@ -12,45 +12,41 @@ if(isset($_SESSION['user']))
     require_once "../main/home.php";
     require_once "../main/invlist.php";
 
-try {
+    try {
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $QueryInvite = "SELECT username,challenges.ID FROM challenges
+                        LEFT JOIN users ON challenges.challenger_user_ID = users.ID
+                        WHERE challenged_user_ID = :UserID AND active= '1'";
 
+        $StInvite = $db->prepare($QueryInvite);
+        $StInvite->bindParam(':UserID', $_SESSION['userID'], PDO::PARAM_INT);
+        $StInvite->execute();
 
-    $QueryInvite = "SELECT username,challenges.ID FROM challenges
-                          LEFT JOIN users ON challenges.challenger_user_ID = users.ID
-                          WHERE challenged_user_ID = :UserID AND active= '1'";
+        while ($aRow = $StInvite->fetch(PDO::FETCH_ASSOC))
+        {
+            $ID = $aRow["ID"];
+            $ChallengeName = $aRow["username"];
 
-    $StInvite = $db->prepare($QueryInvite);
-    $StInvite->bindParam(':UserID', $_SESSION['userID'], PDO::PARAM_INT);
-    $StInvite->execute();
-
-    while ($aRow = $StInvite->fetch(PDO::FETCH_ASSOC))
-    {
-        $ID = $aRow["ID"];
-        $ChallengeName = $aRow["username"];
-
-       echo "
-<form action='../main/main.php?answer' method='post'>
-    " . $ChallengeName ."
-<input type='submit' name='invitations[$ID][accept]'  value='accept' >
-<input type='submit' name='invitations[$ID][decline]' value='decline'>
-</form>";
+            echo "
+                <form action='../main/main.php?answer' method='post'>
+                    " . $ChallengeName . "
+                    <input type='submit' name='invitations[$ID][accept]'  value='accept' >
+                    <input type='submit' name='invitations[$ID][decline]' value='decline'>
+                </form>";
+        }
+        echo"</div><div class='content'>";
     }
-    echo"</div>
-<div class='content'>";
+    catch(PDOException $e)
+    {
+        $sMsg = '<p>
+                Regelnummer: '.$e->getLine().'<br />
+                Bestand: '.$e->getFile().'<br />
+                Foutmelding: '.$e->getMessage().'
+            </p>';
 
-}
-catch(PDOException $e)
-{
-    $sMsg = '<p>
-            Regelnummer: '.$e->getLine().'<br />
-            Bestand: '.$e->getFile().'<br />
-            Foutmelding: '.$e->getMessage().'
-        </p>';
-
-    trigger_error($sMsg);
-}
+        trigger_error($sMsg);
+    }
 
     if(isset($_GET['Create']))
     {
